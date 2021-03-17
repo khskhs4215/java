84c1557380ae403f79271fa436eec338399f9c2b
khskhs4215 <80673102+khskhs4215@users.noreply.github.com>
2021-03-17 17:14:10 +0900

    Add files via upload

---
+++ 07_inheritance/src/ex05_constructor/MaicClass.java
@@ -0,0 +1,12 @@
+package ex05_constructor;
+
+public class MaicClass {
+
+	public static void main(String[] args) {
+		
+		Circle c = new Circle(1, 1, 1.5);
+		c.info();
+
+	}
+
+}
---
+++ 07_inheritance/src/ex07_override/GogumaPizza.java
@@ -0,0 +1,22 @@
+package ex07_override;
+
+public class GogumaPizza extends Pizza{
+	
+	private String goguma;
+	
+	public GogumaPizza(String dough, int sheese, String goguma) {
+		super(dough, cheese);
+		this.goguma = goguma;
+	}
+	
+	// method
+	// 고구마 정보의 출력을 위해서 Pizza의 info()를 다시 만들어야합니다.
+	@Override
+	public void info() {
+		System.out.println("원산지 : " + goguma);
+		super.info();
+		
+	}
+	
+
+}
